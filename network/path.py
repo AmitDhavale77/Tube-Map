91dd81ceb6ab69ef9d9e654f8be061a40ef2fda0
sunitadhavale <sunitadhavale@gmail.com>
2024-10-18 15:29:02 +0100

    added modularity in task 3

--- network/path.py
+++ network/path.py
@@ -87,7 +87,44 @@ class PathFinder:
         return path
 
     def get_shortest_path(self, start_station_name, end_station_name):
-        """Finds the shortest path between two stations using Dijkstra's algorithm."""
+        """ Find ONE shortest path from start_station_name to end_station_name.
+        
+        The shortest path is the path that takes the least amount of time.
+
+        For instance, get_shortest_path('Stockwell', 'South Kensington') 
+        should return the list:
+        [Station(245, Stockwell, {2}), 
+         Station(272, Vauxhall, {1, 2}), 
+         Station(198, Pimlico, {1}), 
+         Station(273, Victoria, {1}), 
+         Station(229, Sloane Square, {1}), 
+         Station(236, South Kensington, {1})
+        ]
+
+        If start_station_name or end_station_name does not exist, return None.
+        
+        You can use the Dijkstra algorithm to find the shortest path from
+        start_station_name to end_station_name.
+
+        Find a tutorial on YouTube to understand how the algorithm works, 
+        e.g. https://www.youtube.com/watch?v=GazC3A4OQTE
+        
+        Alternatively, find the pseudocode on Wikipedia: https://en.wikipedia.org/wiki/Dijkstra's_algorithm#Pseudocode
+
+        Args:
+            start_station_name (str): name of the starting station
+            end_station_name (str): name of the ending station
+
+        Returns:
+            list[Station] : list of Station objects corresponding to ONE 
+                shortest path from start_station_name to end_station_name.
+                Returns None if start_station_name or end_station_name does not 
+                exist.
+                Returns a list with one Station object (the station itself) if 
+                start_station_name and end_station_name are the same.
+        """
+        # TODO: Complete this method
+
         start_id = self.get_station_id(start_station_name)
         end_id = self.get_station_id(end_station_name)
 
